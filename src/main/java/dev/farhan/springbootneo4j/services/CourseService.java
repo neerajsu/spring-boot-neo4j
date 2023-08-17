ceb6efb3a6dd0ecadee189f7ffb1e96781bed29b
neerajsu <neeraj.subramaniyan@gmail.com>
2023-08-17 18:26:44 +0530

    working project

--- src/main/java/dev/farhan/springbootneo4j/services/CourseService.java
+++ src/main/java/dev/farhan/springbootneo4j/services/CourseService.java
@@ -2,9 +2,11 @@ package dev.farhan.springbootneo4j.services;
 
 import dev.farhan.springbootneo4j.models.Course;
 import dev.farhan.springbootneo4j.repositories.CourseRepository;
+import org.springframework.stereotype.Service;
 
 import java.util.List;
 
+@Service
 public class CourseService {
 
     private final CourseRepository courseRepository;
