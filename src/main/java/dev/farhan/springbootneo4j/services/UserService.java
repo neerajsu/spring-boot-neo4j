b90290f294a645db162e989053d7c674ee4244c9
neerajsu <neeraj.subramaniyan@gmail.com>
2023-08-22 11:04:00 +0530

    Check if user exists, before creating user

--- src/main/java/dev/farhan/springbootneo4j/services/UserService.java
+++ src/main/java/dev/farhan/springbootneo4j/services/UserService.java
@@ -3,10 +3,12 @@ package dev.farhan.springbootneo4j.services;
 import dev.farhan.springbootneo4j.models.User;
 import dev.farhan.springbootneo4j.repositories.UserRepository;
 import dev.farhan.springbootneo4j.requests.CreateUserRequest;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
 @Service
+@Slf4j
 public class UserService {
     private final UserRepository userRepository;
     private final PasswordEncoder passwordEncoder;
@@ -19,15 +21,19 @@ public class UserService {
 
     public User createUser(CreateUserRequest createUserRequest) {
 
-        User user = new User();
-        user.setName(createUserRequest.getName());
-        user.setRoles(createUserRequest.getRoles());
-        user.setUsername(createUserRequest.getUsername());
-        user.setPassword(passwordEncoder.encode(createUserRequest.getPassword()));
-
-        userRepository.save(user);
-
-        return user;
+        var userAlreadyPresentOptional = userRepository.findUserByUsername(createUserRequest.getUsername());
+        if (userAlreadyPresentOptional.isEmpty()) {
+            User user = new User();
+            user.setName(createUserRequest.getName());
+            user.setRoles(createUserRequest.getRoles());
+            user.setUsername(createUserRequest.getUsername());
+            user.setPassword(passwordEncoder.encode(createUserRequest.getPassword()));
+            userRepository.save(user);
+            return user;
+        } else {
+            log.info("User " + createUserRequest.getUsername() + " is already present");
+            return userAlreadyPresentOptional.get();
+        }
     }
 
 }
